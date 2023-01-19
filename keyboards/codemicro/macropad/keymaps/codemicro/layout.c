e70c8a1f17bfd40abc0c536bdf4ef222017c642d
AKP <tom@tdpain.net>
2023-01-19 13:31:30 +0000

    Update `layout.c`

--- keyboards/codemicro/macropad/keymaps/codemicro/layout.c
+++ keyboards/codemicro/macropad/keymaps/codemicro/layout.c
@@ -11,8 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x3(
-        KC_TRNS,             KC_TRNS,             KC_TRNS,
-        KC_TRNS,             KC_TRNS,             KC_TRNS,
+        MO(1),               KC_TRNS,             LCTL(KC_F2),
+        SGUI(KC_SPACE),      LALT(KC_A),          LSFT(KC_F10),
         KC_MEDIA_PLAY_PAUSE, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK
+    ),
+    [1] = LAYOUT_ortho_3x3(
+	KC_TRNS, KC_TRNS, KC_TRNS,
+	KC_TRNS, KC_TRNS, LSFT(KC_F9),
+	KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
